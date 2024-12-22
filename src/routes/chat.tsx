bc6920c4a88cb9eb14afcebdd754bb70c9c89452
Diogo L Scarmagnani <diogols@live.com>
2024-12-22 12:22:02 -0300

    starting implementing tanstack form

--- src/routes/chat.tsx
+++ src/routes/chat.tsx
@@ -1,7 +1,8 @@
 import { createFileRoute, redirect } from '@tanstack/react-router'
+import { useForm } from '@tanstack/react-form'
 import { useMutation, useQuery } from 'convex/react'
 import { api } from '../../convex/_generated/api'
-import { useEffect, useState } from 'react'
+import { useEffect } from 'react'
 
 export const Route = createFileRoute('/chat')({
   component: Chat,
@@ -10,24 +11,101 @@ export const Route = createFileRoute('/chat')({
       return redirect({ to: '/', replace: true })
     }
   },
-  loader: ({ context }) => context?.user!,
+  loader({ context }) {
+    return context?.user!
+  }
 })
 
-function Chat() {
-  const user = Route.useLoaderData()
-  
-  const messages = useQuery(api.chat.getMessages);
-  
-  const sendMessage = useMutation(api.chat.sendMessage);
-
-  const [newMessageText, setNewMessageText] = useState('');
+function Messages({ currentUserId }: { currentUserId: string }) {
+  const messages = useQuery(api.chat.getMessages)
 
   useEffect(() => {
     // Make sure scrollTo works on button click in Chrome
     setTimeout(() => {
-      window.scrollTo({ top: document.body.scrollHeight, behavior: "smooth" });
-    }, 0);
-  }, [messages]);
+      window.scrollTo({ top: document.body.scrollHeight, behavior: "smooth" })
+    }, 0)
+  }, [messages])
+
+  return (
+    <>
+      {messages?.map((message) => (
+        <article
+          key={message._id}
+          className={message.userId === currentUserId ? 'message-mine' : ''}
+        >
+          <div>{message.user}</div>
+
+          <p>{message.body}</p>
+        </article>
+      ))}
+    </>
+  )
+}
+
+function FormNewMessage({ userFirstName, userId }: { userFirstName: string, userId: string }) {
+  const sendMessage = useMutation(api.chat.sendMessage)
+
+  const form = useForm({
+    defaultValues: {
+      body: '',
+    },
+    onSubmit: async ({ value }) => {
+      await sendMessage({ user: userFirstName, userId, body: value.body })
+    },
+  })
+  
+  return (
+    <form
+      onSubmit={async (e) => {
+        e.preventDefault()
+        e.stopPropagation()
+        form.handleSubmit()
+      }}
+    >
+      <form.Field
+        name="body"
+        validators={{
+          onChange({ value }) {
+            if (!value) {
+              return 'Message must not be empty'
+            }
+
+            if (value?.trim() === '') {
+              return 'Message must not contain only whitespaces'
+            }
+
+            if (value.length > 255) {
+              return 'Message must not have more than 255 characters'
+            }
+
+            return undefined
+          }
+        }}
+        children={(field) => (
+          <>
+            <input
+              name={field.name}
+              value={field.state.value}
+              onBlur={field.handleBlur}
+              onChange={e => field.handleChange(e.target.value)}
+              placeholder="Write a message…"
+            />
+            {field.state.meta.errors && (
+              <p className="text-red-500 text-sm">{field.state.meta.errors.join(', ')}</p>
+            )}
+          </>
+        )}
+      />
+
+      <button type="submit" disabled={form.state.isSubmitting}>
+        Send
+      </button>
+    </form>
+  )
+}
+
+function Chat() {
+  const user = Route.useLoaderData()
   
   return (
     <>
@@ -38,35 +116,10 @@ function Chat() {
             Connected as <strong>{user.firstName}</strong>
           </p>
         </header>
-        {messages?.map((message) => (
-          <article
-            key={message._id}
-            className={message.userId === user.id ? "message-mine" : ""}
-          >
-            <div>{message.user}</div>
 
-            <p>{message.body}</p>
-          </article>
-        ))}
-        <form
-          onSubmit={async (e) => {
-            e.preventDefault();
-            await sendMessage({ user: user.firstName!, userId: user.id, body: newMessageText });
-            setNewMessageText("");
-          }}
-        >
-          <input
-            value={newMessageText}
-            onChange={async (e) => {
-              const text = e.target.value;
-              setNewMessageText(text);
-            }}
-            placeholder="Write a message…"
-          />
-          <button type="submit" disabled={!newMessageText}>
-            Send
-          </button>
-        </form>
+        <Messages currentUserId={user.id} />
+        
+        <FormNewMessage userFirstName={user.firstName!} userId={user.id} />
       </div>
     </>
   );
